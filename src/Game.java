62c9f44fdd173677c2640add626927bc6d69da6b
Tori Ann Williams <twilli2550@gmail.com>
2019-05-29 08:38:01 -0400

    commit

--- src/Game.java
+++ src/Game.java
@@ -8,9 +8,13 @@ public class Game {
   private WavPlayer song1;
   private WavPlayer song2;
   private String mainWitch;
+  private int lives;
+  private boolean readyToStart;
 
   public Game() {
 
+    readyToStart = true;
+    lives = 5;
     mainWitch = "witch.png";
     grid = new Grid(5, 10);
     userRow = 0;
@@ -20,7 +24,7 @@ public class Game {
     updateTitle();
     grid.setImage(new Location(userRow, 0), "user.gif");
     song2 = new WavPlayer("CB money.wav");
-    song2.startSound();
+    // song2.startSound();
     grid.setBackground("mainpic.jpg");
     grid.setImage(new Location(userRow, 0), mainWitch);
   }
@@ -35,6 +39,12 @@ public class Game {
         scrollLeft();
         populateRightEdge();
       }
+
+      if (readyToStart && userRow == 2) {
+        readyToStart = false;
+        handleCollision(new Location(0, 0));
+      }
+
       updateTitle();
       msElapsed += 100;
     }
@@ -79,6 +89,10 @@ public class Game {
   }
 
   public void handleCollision(Location loc) {
+
+    song2.startSound();
+    lives--;
+    System.out.println("Lives:" + lives);
 
   }
 
