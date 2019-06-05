bbbc1e1892177b0eb3399ddb2187467a2eb91d90
Randy Garcia <garciarandy701@gmail.com>
2019-06-05 09:02:58 -0400

    HandleCollision is almost fully up and working

--- src/Game.java
+++ src/Game.java
@@ -137,6 +137,7 @@ public class Game {
         } else if (notesPic.equals(grid.getImage(temp))) {
           Location newLoc = new Location(i, j - 1);
           grid.setImage(newLoc, notesPic);
+          handleCollision(newLoc);
           grid.setImage(temp, null);
         }
 
@@ -145,7 +146,16 @@ public class Game {
   }
 
   public void handleCollision(Location loc) {
-    //if 
+    int tempR = loc.getRow();
+    int tempC = loc.getCol();
+    boolean collision = false;
+    if(grid.getImage(loc) != null) {
+      if(tempR == userRow && tempC == 0){
+        collision = true;
+        grid.pause(1000);
+      }
+    }
+    System.out.println(collision);
   }
 
   public int getScore() {
@@ -163,5 +173,6 @@ public class Game {
   public static void main(String[] args) {
     Game game = new Game();
     game.play();
+
   }
 }
